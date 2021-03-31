a710a71c9c18b013ab8f26a2d436defc910ca02f
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2021-03-31 01:52:37 -0400

    fix(defup): keep order when deduplicating string

--- functions/defup.fish
+++ functions/defup.fish
@@ -1,3 +1,3 @@
 function defup --description "Deduplicate all strings and return delimited by new line"
-    printf '%s ' $argv | string replace -ar '\s+' ' ' | string trim | string split ' ' | sort -u
+    printf '%s ' $argv | string replace -ar '\s+' ' ' | string trim | string split ' ' | awk '!uniq[$0]++'
 end
